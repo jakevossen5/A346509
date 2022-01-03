4f8e99d309eb55a816c03bb0ede88f309ab4850a
Jake Vossen <jake@vossen.dev>
2022-01-03 15:55:32 -0700

    put in hashset again

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,20 @@ fn a34_up_to(limit: usize) -> usize {
     let a_limit = (limit / 11) + 1;
     let a_steps: Vec<usize> = (11..a_limit).step_by(10).collect();
     // println!("a setps: {}", a_steps.len());
-    let result: FxHashSet<usize> = a_steps.into_par_iter().map(|a| {
+    let result: HashSet<usize> = a_steps.into_par_iter().map(|a| {
         let b_limit = (limit / a) + 1;
         let b_steps = (a..b_limit).step_by(10);
         (a, b_steps)
-    }).map(|(a, b_steps)| {
+    })
+    .filter(|(_, b)| b.len() != 0 )
+    .map(|(a, b_steps)| {
         b_steps.map(move |y| (a, y)).collect::<Vec<(usize, usize)>>()
     }).flatten().map(|(a, b)| a * b).collect();
 
     result.len()
+
+    // result.par_sort_unstable();
+    // result.dedup();
+
+    // result.len()
 }
