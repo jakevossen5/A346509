3b6ac98548bf0d9deef580f5232837ff7d0f08cc
Jake Vossen <jake@vossen.dev>
2022-01-03 15:00:19 -0700

    use fxhash

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{
     result,
 };
 
+use fxhash::FxHashSet;
 use rayon::{iter::{IntoParallelRefIterator, IntoParallelIterator, ParallelIterator}, slice::ParallelSliceMut};
 
 fn main() {
@@ -24,8 +25,8 @@ fn a34_up_to(limit: usize) -> usize {
 
     let a_limit = (limit / 11) + 1;
     let a_steps: Vec<usize> = (11..a_limit).step_by(10).collect();
-    let result: HashSet<usize> = a_steps.into_par_iter().map(|a| {
-        let mut result = HashSet::new();
+    let result: FxHashSet<usize> = a_steps.into_par_iter().map(|a| {
+        let mut result = FxHashSet::default();
         let b_limit = (limit / a) + 1;
         for b in (a..b_limit).step_by(10) {
             result.insert(a * b);
